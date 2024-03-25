8a15840c78a2e44223c08557470a289d774192dd
Tobias <tobhei-8@student.ltu.se>
2024-03-25 11:35:35 +0100

    added jwt support

--- app.py
+++ app.py
@@ -1,34 +1,47 @@
-from flask import Flask,request
+from flask import Flask, request, jsonify
 import joblib
+
+import jwt
+from datetime import timezone
+from datetime import datetime
+from datetime import timedelta
+from jwt.exceptions import InvalidSignatureError
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 import pickle
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-  return 'Hello World!'
+    return 'Hello World!'
+
 
 @app.route('/weather/predict', methods=['GET'])
 def predict_weather():
-  inparray = [request.json["precipitation"],request.json["temp_max"],request.json["temp_min"],request.json["wind"]]
-  filename = 'finalized_model.sav'
-  loaded_model = joblib.load(filename)
-  result = loaded_model.predict([inparray])
-  print(result)
-  if(result == 1):
-    weather = "rain"
-  else:
-    weather = "sunny"
-  return {"weather":weather}
+    inparray = [request.json["precipitation"], request.json["temp_max"], request.json["temp_min"], request.json["wind"]]
+    filename = 'finalized_model.sav'
+    loaded_model = joblib.load(filename)
+    result = loaded_model.predict([inparray])
+    print(result)
+    if (result == 1):
+        weather = "rain"
+    else:
+        weather = "sunny"
+    return {"weather": weather}
+
 
 @app.route('/weather/accuracy', methods=['GET'])
 def weather_accuracy():
-  return {"accuracy":.8464163822525598}
+    return {"accuracy": .8464163822525598}
+
 
 @app.route('/music/predict', methods=['GET'])
 def predict_music():
+    if verifyToken(request.headers) == False:
+        return "Invalid token", 403
+
     inparray = [request.json["danceability"],
                 request.json["key"],
                 request.json["loudness"],
@@ -45,22 +58,45 @@ def predict_music():
     result = loaded_model.predict([inparray])
     print(result)
     replace_dict = {
-      0:'pop',
-      1:'rap',
-      2:'rock',
-      3:'latin',
-      4:'r&b',
-      5:'edm'
+        0: 'pop',
+        1: 'rap',
+        2: 'rock',
+        3: 'latin',
+        4: 'r&b',
+        5: 'edm'
     }
     music = replace_dict[0]
-    return {"music genre":music}
+    return {"music genre": music}
+
 
 @app.route('/music/accuracy', methods=['GET'])
 def music_accuracy():
-  return {"accuracy":.5509365006852444}
+    return {"accuracy": .5509365006852444}
 
 
+
+
+secret = "BYT UT MOT NÅGOT HÄMLIGT"
+def verifyToken(headers):
+    token = headers.get('Authorization')[7:]
+    print(token)
+    try:
+        encoded = jwt.decode(token, secret, algorithms=['HS256'], verify=True)
+        return encoded
+    except Exception:
+        return False
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    username = request.json['username']
+    password = request.json['password']
+    encoded = jwt.encode({'username': username,
+                          "exp": datetime.now(tz=timezone.utc) + timedelta(days=30),
+                          "iat": datetime.now(tz=timezone.utc)
+                          }, secret, algorithm='HS256')
+    return {"token": encoded}
 
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
